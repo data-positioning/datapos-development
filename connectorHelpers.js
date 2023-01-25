20d4de1a5ea7949d382e968c98af0855428be703
Jonathan Terrell <terrell.jm@gmail.com>
2023-01-25 23:14:15 +0100

    Release v0.0.45

--- connectorHelpers.js
+++ connectorHelpers.js
@@ -10,75 +10,78 @@
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 async function uploadConnector(grunt, fetchModule, config, settings) {
-    // try {
-    //     // Sign in to firebase.
-    //     const firebaseSignInResponse = await fetchModule.default(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${settings.firebaseAPIKey}`, {
-    //         body: JSON.stringify({ email: settings.firebaseEmailAddress, password: settings.firebasePassword, returnSecureToken: true }),
-    //         headers: { Referer: `${settings.firebaseProjectId}.web.app` },
-    //         method: 'POST'
-    //     });
-    //     if (!firebaseSignInResponse.ok) {
-    //         console.log(firebaseSignInResponse.status, firebaseSignInResponse.statusText, await firebaseSignInResponse.text());
-    //         return false;
-    //     }
-    //     const firebaseSignInResult = await firebaseSignInResponse.json();
-    //     console.log('Authenticated with Firebase.');
-    //     // Retrieve description and logo.
-    //     const description = grunt.file.read('./src/description.md');
-    //     const logo = grunt.file.read('./src/logo.svg');
-    //     // Upsert connector record in application service database (firestore).
-    //     const firebaseUpsertResponse = await fetchModule.default(`https://europe-west1-${settings.firebaseProjectId}.cloudfunctions.net/api/plugins`, {
-    //         body: JSON.stringify(getConnectorConfig(config, grunt.config.data.pkg.version, description, logo)),
-    //         headers: { Authorization: firebaseSignInResult.idToken, 'Content-Type': 'application/json' },
-    //         method: 'POST'
-    //     });
-    //     if (!firebaseUpsertResponse.ok) {
-    //         console.log(firebaseUpsertResponse.status, firebaseUpsertResponse.statusText, await firebaseUpsertResponse.text());
-    //         return false;
-    //     }
-    //     console.log('Loaded connector instance to Firestore database.');
-    //     // ...
-    //     let sanityImageId = undefined;
-    //     if (logo) {
-    //         const uploadSanityImageResponse = await fetchModule.default(buildSanityURL('assets/images', settings), {
-    //             headers: { Authorization: `Bearer ${settings.sanityAPIToken}`, 'Content-Type': 'image/jpeg' },
-    //             body: logo,
-    //             method: 'POST'
-    //         });
-    //         if (!uploadSanityImageResponse.ok) {
-    //             console.log(uploadSanityImageResponse.status, uploadSanityImageResponse.statusText, await uploadSanityImageResponse.text());
-    //             return false;
-    //         }
-    //         sanityImageId = (await uploadSanityImageResponse.json()).document._id;
-    //         console.log('Uploaded image to Sanity assets.');
-    //     }
-    //     // Upsert Sanity document.
-    //     const createOrReplace = {
-    //         _id: config.id,
-    //         _type: 'dataStore',
-    //         category: config.categoryId,
-    //         description,
-    //         icon: sanityImageId ? { asset: { _ref: sanityImageId }, _type: 'reference' } : undefined,
-    //         label: config.label,
-    //         logo,
-    //         status: config.statusId,
-    //         usage: config.usageId
-    //     };
-    //     const sanityUpsertResponse = await fetchModule.default(buildSanityURL('data/mutate', settings), {
-    //         body: JSON.stringify({ mutations: [{ createOrReplace }] }),
-    //         headers: { Authorization: `Bearer ${settings.sanityAPIToken}`, 'Content-Type': 'application/json' },
-    //         method: 'POST'
-    //     });
-    //     if (!sanityUpsertResponse.ok) {
-    //         console.log(sanityUpsertResponse.status, sanityUpsertResponse.statusText, await sanityUpsertResponse.text());
-    //         return false;
-    //     }
-    //     console.log('Loaded connector document to Sanity dataset.');
-    //     return true;
-    // } catch (error) {
-    //     console.log(error);
-    //     return false;
-    // }
+    try {
+        // Sign in to firebase.
+        const firebaseSignInResponse = await fetchModule.default(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${settings.firebaseAPIKey}`, {
+            body: JSON.stringify({ email: settings.firebaseEmailAddress, password: settings.firebasePassword, returnSecureToken: true }),
+            headers: { Referer: `${settings.firebaseProjectId}.web.app` },
+            method: 'POST'
+        });
+        if (!firebaseSignInResponse.ok) {
+            console.log(firebaseSignInResponse.status, firebaseSignInResponse.statusText, await firebaseSignInResponse.text());
+            return false;
+        }
+        const firebaseSignInResult = await firebaseSignInResponse.json();
+        console.log('Authenticated with Firebase.');
+
+        // Retrieve description and logo.
+        const description = grunt.file.read('./src/description.md');
+        const logo = grunt.file.read('./src/logo.svg');
+
+        // Upsert connector record in application service database (firestore).
+        const firebaseUpsertResponse = await fetchModule.default(`https://europe-west1-${settings.firebaseProjectId}.cloudfunctions.net/api/plugins`, {
+            body: JSON.stringify(getConnectorConfig(config, grunt.config.data.pkg.version, description, logo)),
+            headers: { Authorization: firebaseSignInResult.idToken, 'Content-Type': 'application/json' },
+            method: 'POST'
+        });
+        if (!firebaseUpsertResponse.ok) {
+            console.log(firebaseUpsertResponse.status, firebaseUpsertResponse.statusText, await firebaseUpsertResponse.text());
+            return false;
+        }
+        console.log('Loaded connector instance to Firestore database.');
+
+        //     // ...
+        //     let sanityImageId = undefined;
+        //     if (logo) {
+        //         const uploadSanityImageResponse = await fetchModule.default(buildSanityURL('assets/images', settings), {
+        //             headers: { Authorization: `Bearer ${settings.sanityAPIToken}`, 'Content-Type': 'image/jpeg' },
+        //             body: logo,
+        //             method: 'POST'
+        //         });
+        //         if (!uploadSanityImageResponse.ok) {
+        //             console.log(uploadSanityImageResponse.status, uploadSanityImageResponse.statusText, await uploadSanityImageResponse.text());
+        //             return false;
+        //         }
+        //         sanityImageId = (await uploadSanityImageResponse.json()).document._id;
+        //         console.log('Uploaded image to Sanity assets.');
+        //     }
+        //     // Upsert Sanity document.
+        //     const createOrReplace = {
+        //         _id: config.id,
+        //         _type: 'dataStore',
+        //         category: config.categoryId,
+        //         description,
+        //         icon: sanityImageId ? { asset: { _ref: sanityImageId }, _type: 'reference' } : undefined,
+        //         label: config.label,
+        //         logo,
+        //         status: config.statusId,
+        //         usage: config.usageId
+        //     };
+        //     const sanityUpsertResponse = await fetchModule.default(buildSanityURL('data/mutate', settings), {
+        //         body: JSON.stringify({ mutations: [{ createOrReplace }] }),
+        //         headers: { Authorization: `Bearer ${settings.sanityAPIToken}`, 'Content-Type': 'application/json' },
+        //         method: 'POST'
+        //     });
+        //     if (!sanityUpsertResponse.ok) {
+        //         console.log(sanityUpsertResponse.status, sanityUpsertResponse.statusText, await sanityUpsertResponse.text());
+        //         return false;
+        //     }
+        //     console.log('Loaded connector document to Sanity dataset.');
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
 }
 
 module.exports = { uploadConnector };

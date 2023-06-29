e02f1906ee800983b61862d46501d4223fab9dca
Jonathan Terrell <terrell.jm@gmail.com>
2023-06-29 13:32:29 +0200

    v0.1.190

--- contextHelpers.js
+++ contextHelpers.js
@@ -27,15 +27,23 @@ async function upload(grunt, dataposContextUploadToken, projectId) {
         // const formData = new FormData();
 
         grunt.file.recurse('dist', async (absPath, rootDir, subDir, filename) => {
-            if (subDir) return;
-            const data = grunt.file.read(absPath);
-            // const contentAsBlob = new Blob([data], { type: 'text/plain' });
-            // formData.append(filename, contentAsBlob, filename);
-            console.log(1111);
-            const url = `https://europe-west1-datapos-${projectId}.cloudfunctions.net/api/contexts`;
-            const response = await fetch(url, { method: 'POST', headers: { Authorization: dataposContextUploadToken }, body: JSON.stringify({ name: filename, config: data }) });
-            console.log(9999);
-            if (!response.ok) throw new Error(await response.text());
+            try {
+                if (subDir) return;
+                const data = grunt.file.read(absPath);
+                // const contentAsBlob = new Blob([data], { type: 'text/plain' });
+                // formData.append(filename, contentAsBlob, filename);
+                console.log(1111);
+                const url = `https://europe-west1-datapos-${projectId}.cloudfunctions.net/api/contexts`;
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: { Authorization: dataposContextUploadToken },
+                    body: JSON.stringify({ name: filename, config: data })
+                });
+                console.log(9999);
+                if (!response.ok) throw new Error(await response.text());
+            } catch (error) {
+                console.log(error);
+            }
         });
 
         return true;

9c857408e48b01fd47464740591cae52d3658edc
Jonathan Terrell <terrell.jm@gmail.com>
2023-08-31 17:57:27 +0200

    v0.1.231

---
+++ scriptHelpers.js
@@ -0,0 +1,47 @@
+function syncWithGitHub(filePaths) {
+    const exec = require('child_process').exec;
+    const fs = require('fs');
+    try {
+        for (const filePath of filePaths) {
+            fs.readFile(filePath, 'utf8', (error, data) => {
+                try {
+                    if (error) throw error;
+                    const dataAsJSON = JSON.parse(data);
+                    const versionSegments = dataAsJSON.version.split('.');
+                    const newVersion = `${versionSegments[0]}.${versionSegments[1]}.${Number(versionSegments[2]) + 1}`;
+                    dataAsJSON.version = newVersion;
+                    fs.writeFile(filePath, JSON.stringify(dataAsJSON, undefined, 4), (error) => {
+                        if (error) console.error(error);
+                        exec('git add .', (error, stdout, stderr) => {
+                            if (stderr) {
+                                console.error(stderr);
+                                return;
+                            }
+                            console.log(stdout);
+                            exec(`git commit -m v${newVersion}`, (error, stdout, stderr) => {
+                                if (stderr) {
+                                    console.error(stderr);
+                                    return;
+                                }
+                                console.log(stdout);
+                                exec('git push origin main:main', (error, stdout, stderr) => {
+                                    if (stderr) {
+                                        console.error(stderr);
+                                        return;
+                                    }
+                                    console.log(stdout);
+                                });
+                            });
+                        });
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = { syncWithGitHub };

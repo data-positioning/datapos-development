9fe5345a43794cf8d778fb0d554f95c1fc1ef3ce
Jonathan Terrell <terrell.jm@gmail.com>
2023-09-01 13:38:06 +0200

    v0.1.252

--- scriptHelpers.js
+++ scriptHelpers.js
@@ -6,36 +6,36 @@ const path = require('path');
 
 async function syncWithGitHub() {
     const packageData = await fs.readFile('package.json', 'utf8');
-    const packageAsJSON = JSON.parse(packageData);
-    const versionSegments = packageAsJSON.version.split('.');
+    const packageJSON = JSON.parse(packageData);
+    const versionSegments = packageJSON.version.split('.');
     const newVersion = `${versionSegments[0]}.${versionSegments[1]}.${Number(versionSegments[2]) + 1}`;
-    packageAsJSON.version = newVersion;
-    fs.writeFile('package.json', JSON.stringify(packageAsJSON, undefined, 4));
+    packageJSON.version = newVersion;
+    fs.writeFile('package.json', JSON.stringify(packageJSON, undefined, 4));
     await exec('git add .');
     await exec(`git commit -m v${newVersion}`);
     await exec('git push origin main:main');
 }
 
 async function uploadConnector() {
-    const configAsJSON = JSON.parse(await fs.readFile('src/config.json', 'utf8'));
+    const configJSON = JSON.parse(await fs.readFile('src/config.json', 'utf8'));
     const descriptionEN = await fs.readFile('src/description.en.md', 'utf8');
-    const envAsJSON = JSON.parse(await fs.readFile('.env.local', 'utf8'));
+    const envJSON = JSON.parse(await fs.readFile('.env.local', 'utf8'));
     const logo = await fs.readFile('src/logo.svg', 'utf8');
-    const packageAsJSON = JSON.parse(await fs.readFile('package.json', 'utf8'));
+    const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8'));
 
     const formData = new FormData();
-    formData.append('config', JSON.stringify({ ...configAsJSON, description: { en: descriptionEN }, logo, version: packageAsJSON.version }));
+    formData.append('config', JSON.stringify({ ...configJSON, description: { en: descriptionEN }, logo, version: packageJSON.version }));
     const itemNames = await fs.readdir('dist');
     for (const itemName of itemNames) {
         const itemPath = path.join('dist', itemName);
         const stats = await fs.stat(itemPath);
         if (stats.isDirectory()) continue;
-        const contentAsBlob = new Blob([await fs.readFile(itemPath, 'utf8')], { type: 'text/plain' });
-        formData.append(itemName, contentAsBlob, itemName);
+        const contentBlob = new Blob([await fs.readFile(itemPath, 'utf8')], { type: 'text/plain' });
+        formData.append(itemName, contentBlob, itemName);
     }
 
     const url = `https://api-5ykjycpiha-ew.a.run.app/ping`;
-    // const response = await fetch(url, { method: 'POST', headers: { Authorization: envAsJSON.DATAPOS_CONNECTOR_UPLOAD_TOKEN }, body: formData });
+    // const response = await fetch(url, { method: 'POST', headers: { Authorization: envJSON.DATAPOS_CONNECTOR_UPLOAD_TOKEN }, body: formData });
     const response = await fetch(url, { method: 'GET' });
     if (!response.ok) throw new Error(await response.text());
     console.log(await response.json());

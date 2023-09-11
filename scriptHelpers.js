94a6695f05deebef505b83e6190b534c829ad46a
Jonathan Terrell <terrell.jm@gmail.com>
2023-09-11 18:35:11 +0100

    v0.1.264

--- scriptHelpers.js
+++ scriptHelpers.js
@@ -5,6 +5,190 @@ const exec = util.promisify(require('child_process').exec);
 const fs = require('fs').promises;
 const path = require('path');
 
+let contextConfig;
+let focusConfig;
+let issueCount = 0;
+let modelConfig;
+
+async function buildContext() {
+    const contextData = await fs.readFile('./src/data.json', 'utf8');
+    contextConfig = { label: contextData.label, typeId: 'context', focuses: [] };
+    await buildContextPrepare();
+    await buildContextOutput();
+    if (issueCount > 0) console.log(`WARNING: ${issueCount} issues(s) encountered.`);
+}
+
+const buildContextPrepare = async () => {
+    const itemPaths = await fs.readdir('./src');
+
+    itemPaths.forEach((itemPath) => {
+        console.log(1111, itemPath);
+        //     if (grunt.file.isDir(itemPath)) {
+        //         const itemPathSegments = itemPath.split('/');
+        //         if (itemPathSegments.length === 3) {
+        //             const focusId = itemPathSegments[2];
+        //             const focusData = readJSONFile(grunt, `${itemPath}/data.json`);
+        //             focusData.description = readMarkdownFile(grunt, `${itemPath}/description.en.md`);
+        //             focusConfig = { id: focusId, label: focusData.label, description: { en: focusData.description }, typeId: 'focus', models: [] };
+        //             contextConfig.focuses.push(focusConfig);
+        //             processDirectory(grunt, itemPath);
+        //         } else if (itemPathSegments.length === 4) {
+        //             const modelId = itemPathSegments[3];
+        //             const modelData = readJSONFile(grunt, `${itemPath}/data.json`);
+        //             modelData.description = readMarkdownFile(grunt, `${itemPath}/description.en.md`);
+        //             console.log(modelId, modelData, modelData.description);
+        //             modelConfig = { id: modelId, label: modelData.label, description: { en: modelData.description }, typeId: 'model', dimensions: [], entities: [], views: [] };
+
+        //             const dimensionPaths = grunt.file.expand(`${itemPath}/dimensions/*.json`);
+        //             dimensionPaths.forEach((dimensionPath) => {
+        //                 const dimensionPathSegments = dimensionPath.split('/');
+        //                 const dimensionId = dimensionPathSegments[5].split('.')[0];
+        //                 const dimensionData = readJSONFile(grunt, `${itemPath}/dimensions/${dimensionId}.json`);
+        //                 dimensionData.description = readMarkdownFile(grunt, `${itemPath}/dimensions/${dimensionId}.en.md`);
+        //                 const dimensionConfig = { id: dimensionId, label: dimensionData.label, description: { en: dimensionData.description }, typeId: 'dimension', levels: [] };
+        //                 modelConfig.dimensions.push(dimensionConfig);
+        //             });
+
+        //             const entityPaths = grunt.file.expand(`${itemPath}/entities/*.json`);
+        //             entityPaths.forEach((entityPath) => {
+        //                 const entityPathSegments = entityPath.split('/');
+        //                 const entityId = entityPathSegments[5].split('.')[0];
+        //                 const entityData = readJSONFile(grunt, `${itemPath}/entities/${entityId}.json`);
+        //                 entityData.description = readMarkdownFile(grunt, `${itemPath}/entities/${entityId}.en.md`);
+        //                 const entityConfig = {
+        //                     id: entityId,
+        //                     label: entityData.label,
+        //                     description: { en: entityData.description },
+        //                     typeId: 'entity',
+        //                     characteristics: [],
+        //                     computations: [],
+        //                     events: []
+        //                 };
+        //                 for (const characteristic of entityData.characteristics || []) {
+        //                     const characteristicConfig = {
+        //                         entityTypeId: characteristic.entityTypeId,
+        //                         id: characteristic.id,
+        //                         label: characteristic.label,
+        //                         description: characteristic.description,
+        //                         typeId: 'characteristic',
+        //                         type: characteristic.type
+        //                     };
+        //                     entityConfig.characteristics.push(characteristicConfig);
+        //                 }
+        //                 for (const computation of entityData.computations || []) {
+        //                     const computationConfig = {
+        //                         id: computation.id,
+        //                         label: computation.label,
+        //                         description: computation.description,
+        //                         typeId: 'computation',
+        //                         formula: computation.formula
+        //                     };
+        //                     entityConfig.computations.push(computationConfig);
+        //                 }
+        //                 for (const event of entityData.events || []) {
+        //                     const eventConfig = {
+        //                         id: event.id,
+        //                         label: event.label,
+        //                         description: event.description,
+        //                         typeId: 'event'
+        //                     };
+        //                     entityConfig.events.push(eventConfig);
+        //                 }
+        //                 modelConfig.entities.push(entityConfig);
+        //             });
+
+        //             focusConfig.models.push(modelConfig);
+        //         } else {
+        //             throw new Error(`Unexpected directory level: ${itemPath}.`);
+        //         }
+        //     }
+    });
+};
+
+const buildContextOutput = () => {
+    const characteristics = [];
+    const computations = [];
+    const dimensions = [];
+    const entities = [];
+    const events = [];
+    const models = [];
+    const views = [];
+
+    // const context = { id: 'datapos-context-default', label: contextConfig.label, typeId: contextConfig.typeId, focuses: [] };
+    // for (const focus of contextConfig.focuses) {
+    //     const focusId = `datapos-context-default-focus-${focus.id}`;
+    //     const focusReference = { id: focusId, label: focus.label, models: [] };
+    //     for (const model of focus.models) {
+    //         const modelId = `datapos-context-default-model-${model.id}`;
+    //         const modelConfig = {
+    //             id: modelId,
+    //             label: model.label,
+    //             description: model.description,
+    //             typeId: model.typeId,
+    //             dimensions: [],
+    //             entities: [],
+    //             views: []
+    //         };
+
+    //         for (const dimension of model.dimensions) {
+    //             const dimensionId = `datapos-context-default-dimension-${dimension.id}`;
+    //             const dimensionConfig = {
+    //                 id: dimensionId,
+    //                 label: dimension.label,
+    //                 description: dimension.description,
+    //                 typeId: dimension.typeId,
+    //                 levels: dimension.levels
+    //             };
+    //             grunt.file.write(`./dist/${dimensionId}.json`, JSON.stringify(dimensionConfig));
+    //             const dimensionReference = { id: dimensionId, label: dimension.label };
+    //             modelConfig.dimensions.push(dimensionReference);
+    //             dimensions.push({ ...dimensionReference, modelId: modelId, modelLabel: model.label, focusId: focusId, focusLabel: focus.label });
+    //         }
+
+    //         for (const entity of model.entities) {
+    //             const entityId = `datapos-context-default-entity-${entity.id}`;
+    //             const entityConfig = {
+    //                 id: entityId,
+    //                 label: entity.label,
+    //                 description: entity.description,
+    //                 typeId: entity.typeId,
+    //                 characteristics: entity.characteristics,
+    //                 computations: entity.computations,
+    //                 events: entity.events
+    //             };
+    //             grunt.file.write(`./dist/${entityId}.json`, JSON.stringify(entityConfig));
+    //             const entityReference = { id: entityId, label: entity.label };
+    //             modelConfig.entities.push(entityReference);
+    //             entities.push({ ...entityReference, modelId: modelId, modelLabel: model.label, focusId: focusId, focusLabel: focus.label });
+    //         }
+
+    //         for (const view of model.views) {
+    //             const viewId = `datapos-context-default-view-${view.id}`;
+    //             const viewConfig = {
+    //                 id: viewId,
+    //                 label: view.label,
+    //                 description: view.description,
+    //                 typeId: view.typeId
+    //             };
+    //             grunt.file.write(`./dist/${viewId}.json`, JSON.stringify(viewConfig));
+    //             const viewReference = { id: viewId, label: view.label };
+    //             modelConfig.views.push(viewReference);
+    //             views.push({ ...viewReference, modelId: modelId, modelLabel: model.label, focusId: focusId, focusLabel: focus.label });
+    //         }
+    //         grunt.file.write(`./dist/${modelId}.json`, JSON.stringify(modelConfig));
+    //         const modelReference = { id: modelId, label: model.label };
+    //         focusReference.models.push(modelReference);
+    //         models.push({ ...modelReference, focusId: focusId, focusLabel: focus.label });
+    //     }
+    //     context.focuses.push(focusReference);
+    // }
+    // grunt.file.write('./dist/datapos-context-default.json', JSON.stringify(context));
+    // grunt.file.write('./dist/datapos-context-default-models.json', JSON.stringify(models));
+    // grunt.file.write('./dist/datapos-context-default-dimensions.json', JSON.stringify(dimensions));
+    // grunt.file.write('./dist/datapos-context-default-entities.json', JSON.stringify(entities));
+    // grunt.file.write('./dist/datapos-context-default-views.json', JSON.stringify(views));
+};
+
 async function bumpVersion() {
     const packageData = await fs.readFile('package.json', 'utf8');
     const packageJSON = JSON.parse(packageData);
@@ -77,13 +261,32 @@ async function uploadConnector() {
     }
 
     // TODO: Need to get 'api-5ykjycpiha-ew.a.run.app' or portion of it from token.
-    const url = `https://api-5ykjycpiha-ew.a.run.app/connectors`;
+    const url = 'https://api-5ykjycpiha-ew.a.run.app/connectors';
     const response = await fetch(url, { method: 'POST', headers: { Authorization: process.env.DATAPOS_CONNECTOR_UPLOAD_TOKEN }, body: formData });
     if (!response.ok) throw new Error(await response.text());
 }
 
 async function uploadContext() {
-    console.log('Upload context requested...');
+    const items = [];
+    const itemNames = await fs.readdir('dist');
+    for (const itemName of itemNames) {
+        const itemPath = path.join('dist', itemName);
+        const stats = await fs.stat(itemPath);
+        if (stats.isDirectory()) continue;
+        items.push({ itemPath, itemName });
+    }
+
+    for (const item of items) {
+        console.log('ITEM', item.itemName, item.itemPath);
+        // const data = JSON.parse(await fs.readFile(item.itemPath, 'utf8'));
+        // const url = 'https://api-5ykjycpiha-ew.a.run.app/connectors/contexts';
+        // const response = await fetch(url, {
+        //     method: 'POST',
+        //     headers: { Authorization: process.env.DATAPOS_CONTEXT_UPLOAD_TOKEN, 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({ name: item.itemName.slice(0, -5), config: data })
+        // });
+        // if (!response.ok) throw new Error(await response.text());
+    }
 }
 
-module.exports = { bumpVersion, generateFileStoreIndex, getBackendConfig, getWorkbenchConfig, syncWithGitHub, uploadConnector, uploadContext };
+module.exports = { buildContext, bumpVersion, generateFileStoreIndex, getBackendConfig, getWorkbenchConfig, syncWithGitHub, uploadConnector, uploadContext };

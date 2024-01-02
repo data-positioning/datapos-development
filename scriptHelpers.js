5c29913b7b07823e960579486c51b14f4d3a6d1c
Jonathan Terrell <terrell.jm@gmail.com>
2024-01-02 12:57:06 +0100

    v0.1.387

--- scriptHelpers.js
+++ scriptHelpers.js
@@ -349,7 +349,7 @@ async function downloadContext(contextId, outDir) {
     const contextIndex = await getDoc(doc(db, 'components', contextId));
     fs.writeFile(`${outDir}/contextIndex.json`, JSON.stringify(contextIndex.data()));
 
-    const querySnapshot = await getDocs(query(collection(db, 'componentItems'), where(FieldPath.documentId(), '==', 'datapos-context-default-workforce')));
+    const querySnapshot = await getDocs(query(collection(db, 'componentItems'), where(db.FieldPath.documentId(), '==', 'datapos-context-default-workforce')));
     querySnapshot.forEach((doc) => {
         console.log(8888, doc.id);
     });

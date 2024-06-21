a2c46e5a559cfb38e58884c1add3de1f56e82f4b
Jonathan Terrell <terrell.jm@gmail.com>
2024-06-22 01:00:53 +0200

    v0.1.520

--- scriptHelpers.js
+++ scriptHelpers.js
@@ -71,9 +71,7 @@ async function uploadPlugin() {
         configJSON.logo = await fs.readFile('src/logo.svg', 'utf8');
     } catch (error) {}
 
-    console.log('configJSON', configJSON);
-
-    await pushContentToGithub(packageJSON, env, JSON.stringify(configJSON), 'config.json');
+    await pushContentToGithub(packageJSON, env, JSON.stringify({ id: 1234 }), 'config.json');
 
     for (const itemName of await fs.readdir('dist')) {
         const itemPath = path.join('dist', itemName);

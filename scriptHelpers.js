adbd60512c554e79b4ca4697fa9b309167df7787
Jonathan Terrell <terrell.jm@gmail.com>
2024-06-22 11:20:26 +0200

    v0.1.527

--- scriptHelpers.js
+++ scriptHelpers.js
@@ -33,7 +33,7 @@ async function bumpVersion() {
 
 // Facilitators - Compile Presenter
 async function compilePresenter() {
-    const dataJSON = await readJSONFile('src/data.json', 'utf8');
+    const dataJSON = await readJSONFile('presentations/data.json', 'utf8');
     presenterConfig = { label: dataJSON.label, areas: [] };
     await clearDirectory('dist');
     await compilePresenterFolder('src');

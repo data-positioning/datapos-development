b0c2d7c14e2761758bfd143a2a30a8526881e61c
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-04 18:25:49 +0100

    v0.3.35

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function documentInterface(moduleTypeId) {
     const regex = /^\s{4}(?:async\s+)?(?:private\s+|public\s+|protected\s+)?([A-Za-z_]\w*)\s*\([^)]*\)\s*(?::\s*[^{]+)?\s*\{/gm; // Match class methods and top-level functions.
     const matches = [...indexCode.matchAll(regex)].map((m) => m[1]);
     configJSON.interface = matches;
-    await fs.writeFile(`${directoryPath || ''}config.json`, JSON.stringify(configJSON, undefined, 4));
+    await fs.writeFile('config.json', JSON.stringify(configJSON, undefined, 4));
 }
 
 // Operations - Build public directory index.

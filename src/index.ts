642fbe8e1ae97ecb3af50c4552d49c5061cc0bde
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-07 16:53:31 +0100

    v0.3.46

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,97 @@
 // Dependencies - Vendor.
 import { exec as execCallback } from 'child_process';
 import { promises as fs } from 'fs';
+import { nanoid } from 'nanoid';
 import type { PackageJson } from 'type-fest';
 import { promisify } from 'util';
 
-// Initialisation
-const exec = promisify(execCallback);
-
 // Dependencies - Framework.
+import type { ModuleConfig } from '@datapos/datapos-shared';
 import { CONNECTOR_DESTINATION_OPERATIONS, CONNECTOR_SOURCE_OPERATIONS } from '@datapos/datapos-shared';
 import type { ConnectorModuleConfig, ConnectorModuleOperation, ConnectorModuleUsageId } from '@datapos/datapos-shared';
 import type { ContextModuleConfig, ContextModuleOperation } from '@datapos/datapos-shared';
 import type { InformerModuleConfig, InformerModuleOperation } from '@datapos/datapos-shared';
 import type { PresenterModuleConfig, PresenterModuleOperation } from '@datapos/datapos-shared';
 
+// Interfaces/Types - Entry.
+interface Entry {
+    name: string;
+    typeId: 'folder' | 'object';
+}
+interface FolderEntry extends Entry {
+    childCount: number;
+    typeId: 'folder';
+}
+interface ObjectEntry extends Entry {
+    id: string;
+    lastModifiedAt: number;
+    size: number;
+    typeId: 'object';
+}
+
+// Initialisation
+const exec = promisify(execCallback);
+
+// Utilities - Build configuration.
+async function buildConfig(): Promise<void> {
+    try {
+        console.log('🚀 Building configuration...');
+        const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
+        const configJSON = JSON.parse(await fs.readFile('config.json', 'utf8')) as ModuleConfig;
+        if (packageJSON.name) configJSON.id = packageJSON.name;
+        if (packageJSON.version) configJSON.version = packageJSON.version;
+        await fs.writeFile('config.json', JSON.stringify(configJSON, undefined, 4), 'utf8');
+        console.log('✅ Configuration built.');
+    } catch (error) {
+        console.warn('❌ Error building configuration.', error);
+    }
+}
+
+// Utilities - Build public directory index.
+async function buildPublicDirectoryIndex(id: string): Promise<void> {
+    try {
+        console.log(`🚀 Building public directory index for identifier '${id}'...`);
+        const index: Record<string, Entry[]> = {};
+
+        async function listDirectoryEntriesRecursively(directoryPath: string, names: string[]) {
+            console.log(`⚙️ Processing directory '${directoryPath}'...`);
+            const entries: Entry[] = [];
+            const localDirectoryPath = directoryPath.substring(`public/${id}`.length);
+            index[localDirectoryPath] = entries;
+            for (const name of names) {
+                const itemPath = `${directoryPath}/${name}`;
+                try {
+                    const stats = await fs.stat(itemPath);
+                    if (stats.isDirectory()) {
+                        const nextLevelChildren = await fs.readdir(itemPath);
+                        const folderEntry: FolderEntry = { childCount: nextLevelChildren.length, name: `${name}`, typeId: 'folder' };
+                        entries.push(folderEntry);
+                        await listDirectoryEntriesRecursively(itemPath, nextLevelChildren);
+                    } else {
+                        const objectEntry: ObjectEntry = { id: nanoid(), lastModifiedAt: stats.mtimeMs, name, size: stats.size, typeId: 'object' };
+                        entries.push(objectEntry);
+                    }
+                } catch (error) {
+                    console.error(`Unable to get information for '${name}' in 'buildPublicDirectoryIndex'.`, error);
+                }
+            }
+            entries.sort((left, right) => {
+                const typeComparison = left.typeId.localeCompare(right.typeId);
+                return typeComparison !== 0 ? typeComparison : left.name.localeCompare(right.name);
+            });
+        }
+
+        const toplevelNames = await fs.readdir(`public/${id}`);
+        await listDirectoryEntriesRecursively(`public/${id}`, toplevelNames);
+        await fs.writeFile(`./public/${id}Index.json`, JSON.stringify(index), 'utf8');
+        console.log('✅ Public directory index built.');
+    } catch (error) {
+        console.warn('❌ Error building public directory index.', error);
+    }
+}
+
 // Utilities - Build connector configuration.
-export async function buildConnectorConfig(): Promise<void> {
+async function buildConnectorConfig(): Promise<void> {
     try {
         console.log('🚀 Building connector configuration...');
         const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
@@ -57,7 +133,7 @@ export async function buildConnectorConfig(): Promise<void> {
 }
 
 // Utilities - Build context configuration.
-export async function buildContextConfig(): Promise<void> {
+async function buildContextConfig(): Promise<void> {
     try {
         console.log('🚀 Building context configuration...');
         const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
@@ -80,7 +156,7 @@ export async function buildContextConfig(): Promise<void> {
 }
 
 // Utilities - Build informer configuration.
-export async function buildInformerConfig(): Promise<void> {
+async function buildInformerConfig(): Promise<void> {
     try {
         console.log('🚀 Building informer configuration...');
         const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
@@ -103,7 +179,7 @@ export async function buildInformerConfig(): Promise<void> {
 }
 
 // Utilities - Build presenter configuration.
-export async function buildPresenterConfig(): Promise<void> {
+async function buildPresenterConfig(): Promise<void> {
     try {
         console.log('🚀 Building presenter configuration...');
         const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
@@ -126,7 +202,7 @@ export async function buildPresenterConfig(): Promise<void> {
 }
 
 // Utilities - Bump version.
-export async function bumpVersion(): Promise<void> {
+async function bumpVersion(): Promise<void> {
     try {
         console.log('🚀 Bumping version...');
         const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
@@ -146,8 +222,48 @@ export async function bumpVersion(): Promise<void> {
     }
 }
 
+// Utilities - Clear directory.
+async function clearDirectory(directoryPath: string): Promise<void> {
+    try {
+        console.log('🚀 Clearing directory...');
+        for (const itemName of await fs.readdir(directoryPath)) {
+            const itemPath = `${directoryPath}/${itemName}`;
+            const stats = await fs.stat(itemPath);
+            if (stats.isDirectory()) {
+                await fs.rm(itemPath, { recursive: true, force: true });
+            } else {
+                await fs.unlink(itemPath);
+            }
+        }
+        console.log('✅ Directory cleared.');
+    } catch (error) {
+        console.warn('❌ Error bumping package version.', error);
+    }
+}
+
+// Utilities - Send deployment notice.
+async function sendDeploymentNotice(): Promise<void> {
+    try {
+        console.log('🚀 Sending deployment notice...');
+        const configJSON = JSON.parse(await fs.readFile('config.json', 'utf8')) as ModuleConfig;
+        const options = {
+            body: JSON.stringify(configJSON),
+            headers: { 'Content-Type': 'application/json' },
+            method: 'PUT'
+        };
+        const response = await fetch(`https://api.datapos.app/states/${configJSON.id}`, options);
+        if (!response.ok) {
+            console.log(await response.text());
+            throw new Error('Fetch error.');
+        }
+        console.log('✅ Deployment notice sent.');
+    } catch (error) {
+        console.warn('❌ Error sending deployment notice.', error);
+    }
+}
+
 // Utilities - Synchronise with GitHub.
-export async function syncWithGitHub(): Promise<void> {
+async function syncWithGitHub(): Promise<void> {
     try {
         console.log('🚀 Synchronising with GitHub....');
         const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
@@ -159,3 +275,91 @@ export async function syncWithGitHub(): Promise<void> {
         console.warn('❌ Error synchronising with GitHub.', error);
     }
 }
+
+// Utilities - Upload directory to Cloudflare R2.
+async function uploadDirectoryToR2(sourceDirectory: string, uploadDirectory: string) {
+    try {
+        console.log('🚀 Uploading directory to R2....');
+        async function listDirectoryEntriesRecursively(currentSourceDirectory: string, currentDestinationDirectory: string, names: string[]) {
+            for (const name of names) {
+                const sourceItemPath = `${currentSourceDirectory}/${name}`;
+                const destinationItemPath = `${currentDestinationDirectory}/${name}`;
+                try {
+                    const stats = await fs.stat(sourceItemPath);
+                    if (stats.isDirectory()) {
+                        const nextLevelChildren = await fs.readdir(sourceItemPath);
+                        await listDirectoryEntriesRecursively(sourceItemPath, destinationItemPath, nextLevelChildren);
+                    } else {
+                        const command = `wrangler r2 object put "datapos-sample-data-eu/${currentDestinationDirectory}/${name}" --file="${currentSourceDirectory}/${name}" --jurisdiction=eu --remote`;
+                        const response = await exec(command);
+                        console.log('Uploading:', `${currentSourceDirectory}/${name}`);
+                        if (response.stderr) {
+                            console.log('Command___:', command);
+                            console.log('Error_____:', response.stderr);
+                        }
+                    }
+                } catch (error) {
+                    console.error(`Unable to get information for '${name}' in 'uploadDirectoryToR2'.`, error);
+                }
+            }
+        }
+        const toplevelNames = await fs.readdir(`${sourceDirectory}/${uploadDirectory}/`);
+        await listDirectoryEntriesRecursively(`${sourceDirectory}/${uploadDirectory}`, uploadDirectory, toplevelNames);
+        console.log('✅ Directory cleared.');
+    } catch (error) {
+        console.warn('❌ Error uploading directory to R2.', error);
+    }
+}
+
+// Utilities - Upload module configuration.
+async function uploadModuleConfig() {
+    try {
+        console.log('🚀 Uploading module configuration....');
+        const configJSON = JSON.parse(await fs.readFile('config.json', 'utf8')) as ModuleConfig;
+        const stateId = configJSON.id;
+        const options = {
+            body: JSON.stringify(configJSON),
+            headers: { 'Content-Type': 'application/json' },
+            method: 'PUT'
+        };
+        const response = await fetch(`https://api.datapos.app/states/${stateId}`, options);
+        if (!response.ok) console.log(await response.text());
+        console.log('✅ Directory cleared.');
+    } catch (error) {
+        console.warn('❌ Error uploading module configuration.', error);
+    }
+}
+
+// Utilities - Upload module to Cloudflare R2.
+async function uploadModuleToR2(fromPath: string, toPath: string) {
+    try {
+        console.log('🚀 Uploading module to R2....');
+        const packageJSON = JSON.parse(await fs.readFile('package.json', 'utf8')) as PackageJson;
+        const toPathWithVersion = toPath.replace(/^(.*?\.)/, `$1v${packageJSON.version}.`);
+        const { stdout, stderr } = await exec(
+            `wrangler r2 object put ${toPathWithVersion} --file=dist/${fromPath} --content-type application/javascript --jurisdiction=eu --remote`
+        );
+        if (stdout) console.log(stdout);
+        if (stderr) console.error(stderr);
+        console.log('✅ Directory cleared.');
+    } catch (error) {
+        console.warn('❌ Error uploading module to R2.', error);
+    }
+}
+
+// Exposures
+export {
+    buildConfig,
+    buildConnectorConfig,
+    buildContextConfig,
+    buildInformerConfig,
+    buildPresenterConfig,
+    buildPublicDirectoryIndex,
+    bumpVersion,
+    clearDirectory,
+    sendDeploymentNotice,
+    syncWithGitHub,
+    uploadDirectoryToR2,
+    uploadModuleConfig,
+    uploadModuleToR2
+};

89075e5b7340b54f9553b7027ac33c794781eade
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-07 17:30:00 +0100

    v0.3.55

--- dist/types/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Development utilities.
- */
-export declare function buildConnectorConfig(): Promise<void>;
-export declare function buildContextConfig(): Promise<void>;
-export declare function buildInformerConfig(): Promise<void>;
-export declare function buildPresenterConfig(): Promise<void>;
-export declare function bumpVersion(): Promise<void>;
-export declare function syncWithGitHub(): Promise<void>;

f4d28ee07a9457e0c2838b42fdc77556bf61e466
Jonathan Terrell <terrell.jm@gmail.com>
2025-12-16 23:31:53 +0100

    v0.3.380

--- src/operations/formatCode.ts
+++ src/operations/formatCode.ts
@@ -12,7 +12,7 @@ async function formatCode(): Promise<void> {
     try {
         logOperationHeader('Format Code');
 
-        await spawnCommand('1️⃣  Format', 'prettier', ['--write', '.']);
+        await spawnCommand('1️⃣  Format', 'prettier', ['--write', '*.json', '*.ts', 'src/**']);
 
         logOperationSuccess('Code formatted.');
     } catch (error) {

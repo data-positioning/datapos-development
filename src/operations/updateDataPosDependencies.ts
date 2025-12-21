e040295eaf4a8af5a16a812b8973482a9b5abfec
Jonathan Terrell <terrell.jm@gmail.com>
2025-12-21 11:35:35 +0100

    v0.3.415

--- src/operations/updateDataPosDependencies.ts
+++ src/operations/updateDataPosDependencies.ts
@@ -79,6 +79,8 @@ async function syncConfigFile(moduleDirectory: string, templateFilePath: string,
     }
 
     if (destinationContent === templateContent) {
+        console.log(1111, destinationContent);
+        console.log(2222, templateContent);
         console.info(`ℹ️  File '${fileName}' is already up to date.`);
         return;
     }
